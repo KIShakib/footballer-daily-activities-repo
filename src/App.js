2d25e9e9e442c0a5865e6d9f19488e43c554dc01
Khubaibul Islam <khubaibulislam2001@gmail.com>
2022-09-28 20:58:42 +0600

    3rd commit & Navbar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div>
-      <h1>Test that everything is connected.</h1>
+      <Navbar></Navbar>
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Navbar.css';
+import logo from '../Images/bg2.png';
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className="navbar bg-base-300 shadow-xl lg:px-20">
+                <div className="flex-1">
+                    <a className="btn btn-ghost normal-case lg:text-2xl" href='/'>Footballer Daily Practice Activities</a>
+                </div>
+                <div className="flex-none">
+                    <div className="dropdown dropdown-end">
+                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img src={logo} alt='Shakib' />
+                            </div>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            <li>
+                                <a className="justify-between" href='/'>
+                                    Profile
+                                    <span className="badge">New</span>
+                                </a>
+                            </li>
+                            <li><a href='/'>Settings</a></li>
+                            <li><a href='/'>Logout</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
